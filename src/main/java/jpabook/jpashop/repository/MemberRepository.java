6c86acd83d9ee47723346aaaa3ced4184613e52f
SheepEatLion <gowjr207@gmail.com>
2022-02-02 17:55:28 +0900

    refactor: final 객체로 변경

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -8,10 +9,12 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    // @PersistenceContext 도 생성자로 주입이 가능함(스프링이라서) 때문에 일관성을 위해 롬복 라이브러리를 쓰고 파이널 변수로 바꾸면됌.
+    private final EntityManager em;
+
 
     public Long save(Member member) {
         em.persist(member);
