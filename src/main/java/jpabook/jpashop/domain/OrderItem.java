82de5afc97f1480efe8d9380b2e1bc25b7b63f68
SheepEatLion <gowjr207@gmail.com>
2022-02-04 13:29:35 +0900

    feat: 주문상품 생성로직

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -26,6 +26,18 @@ public class OrderItem {
     private int orderPrice; // 주문가격
     private int count;      // 주문수량
 
+    // 생성 로직
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+
     // 비지니스 로직
     public void cancel() {
         getItem().addStock(count);
